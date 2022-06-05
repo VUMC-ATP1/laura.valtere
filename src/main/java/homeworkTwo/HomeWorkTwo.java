a205f863e8c6b8c194a317cd7976983b98d22cf0
laurav <laura.walter.ca@gmail.com>
2022-06-05 19:30:29 +0300

    [feature/java]: HomeWorkTwo update

--- src/main/java/homeworkTwo/HomeWorkTwo.java
+++ src/main/java/homeworkTwo/HomeWorkTwo.java
@@ -7,13 +7,13 @@ public class HomeWorkTwo {
         int x = 2;
         System.out.println(x > 0);
 
-        int x = 1;
+        x = 1;
         System.out.println(x < 0);
 
-        int x = 6;
+        x = 6;
         System.out.println(x > 5 && x < 10);
 
-        int x = 5;
+        x = 5;
         System.out.println(x <= 5 && x < 10);
         System.out.println(x == 0 || x == 10);
         System.out.println((x * x) > 10);
@@ -45,7 +45,7 @@ public class HomeWorkTwo {
                 break;
             case 8:
                 System.out.println("August");
-                break:
+                break;
             case 9:
                 System.out.println("September");
                 break;
@@ -61,55 +61,47 @@ public class HomeWorkTwo {
         }
 
 
-
         int a = 1;
         int b = 2;
         int c = 3;
-        if (a > b)
-        System.out.println("a ir lielaks par b un c");
-    if else if (b > c) {
+        if (a > b) {
+            System.out.println("a ir lielaks par b un c");
+        }
+        else if (b > c) {
             System.out.println("b ir lielaks par a un c");
-        }else{
+        } else {
             System.out.println("c ir lielaks par a un b");
         }
 
         Scanner scanner = new Scanner(System.in);
-    String krasa = "Sarkana";
+        String krasa = "Sarkans";
         System.out.println("Ievadi luksafora krasu");
-    krasa = scanner.nextLine();
-    if (krasa == 'Sarkana') {
-        System.out.println(krasa + "gajeniem jastav un gaidit zalo signalu");
-    } else if (krasa == 'Dzeltena') {
-        System.out.println(krasa + "gajeniem vel jagaida zalo signalu");
-    } else if (krasa == 'Zala') {
-        System.out.println("gajenam driks iet pari celai");
-    } else {
-        System.out.println("Luksaforam nav tadas krasas (ievadi: Sarkana,Dzeltena,Zala");
-    }
+        krasa = scanner.nextLine();
+        if (krasa.equals("Sarkans")) {
+            System.out.println(krasa + "gajeniem jastav un gaidit zalo signalu");
+        } else if (krasa.equals("Dzeltens")) {
+            System.out.println(krasa + "gajeniem vel jagaida zalo signalu");
+        } else if (krasa.equals("Zals")) {
+            System.out.println("gajenam driks iet pari celai");
+        } else {
+            System.out.println("Luksaforam nav tadas krasas (ievadi: Sarkans,Dzeltens,Zals");
+        }
 
+        printBusinessCard("Laura", "Valtere", 28208207, 1990);
+        printBusinessCard("Laura", "Valtere", 28208207, 1990);
+        printBusinessCard("Laura", "Valtere", 28208207, 1990);
+        printBusinessCardTwo("Kapusta", "Salat", 28938726, 1999);
+        printBusinessCardTwo("Liga", "Kalnina", 28733651, 1987);
+        sum(4, 10);
+        average(10, 2, 4);
 
-    //krasa.equals("Sarkans");
-    //krasa.equals("Dzeltens");
-    //krasa.equals("Zals");
-    }
-
-    public static void main(String[] args) {
-        printBusinessCard("Laura", "Valtere", 28208207,1990);
-        printBusinessCard("Laura", "Valtere", 28208207,1990);
-        printBusinessCard("Laura", "Valtere", 28208207,1990);
-
-    }
+        }
 
     public static void printBusinessCard(String name, String surname, int number, int year) {
         System.out.printf("Vards: %s \n", name);
         System.out.printf("Uzvards: %s \n", surname);
         System.out.printf("Numurs: %d \n", number);
         System.out.printf("Dzimsanas gads: %d \n", year);
-    }
-
-    public static void main(String[] args) {
-        printBusinessCardTwo("Kapusta", "Salat", 28938726,1999);
-        printBusinessCardTwo("Liga", "Kalnina", 28733651, 1987);
     }
 
     public static void printBusinessCardTwo(String name, String surname, int number, int year) {
@@ -119,19 +111,13 @@ public class HomeWorkTwo {
         System.out.printf("Dzimsanas gads: %d \n", year);
     }
 
-    public static void main(String[] args) {
-        sum(4,10);
-        average(10,2,4);
+
+    public static void sum(int x, int y) {
     }
 
 
-    public static int sum(int x, int y) {
-        return x*y;
+    public static void average(double a, double b, double c) {
+        return;
     }
 
-
-    public static double average(double a, double b, double c) {
-        return double;
-            }
-            
 }
